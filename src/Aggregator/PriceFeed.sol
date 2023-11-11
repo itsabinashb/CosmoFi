7d35a9d5779dfdca5cefd806601486e478269634
Abinash Burman <itsabinashb@gmail.com>
2023-11-11 17:12:44 +0530

    Commit

--- src/Aggregator/PriceFeed.sol
+++ src/Aggregator/PriceFeed.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
-
+//import "@openzeppelin/contracts/";
 abstract contract PriceFeed {
 
-    function setAggregatorAddress(address _aggregator) external 
+  function setAggregatorAddress(address _aggregator) external {}
 }
