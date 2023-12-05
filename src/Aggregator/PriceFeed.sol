683b865d5858ddb38860e4499afc3bd1935c525c
Abinash Burman <itsabinashb@gmail.com>
2023-12-05 23:48:51 +0530

    mission - 1 done

--- src/Aggregator/PriceFeed.sol
+++ src/Aggregator/PriceFeed.sol
@@ -3,13 +3,18 @@ pragma solidity 0.8.20;
 import 'chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 
 abstract contract PriceFeed {
-  AggregatorV3Interface public aggregator;
+  AggregatorV3Interface public aggregatorBTC;
+  AggregatorV3Interface public aggregatorDAI;
 
-  function setAggregatorAddress(AggregatorV3Interface _aggregator) internal {
-    aggregator = AggregatorV3Interface(_aggregator);
+  function setAggregatorAddressForBTC(AggregatorV3Interface _aggregator) internal {
+    aggregatorBTC = AggregatorV3Interface(_aggregator);
   }
 
-  function getPrice() internal view returns (int) {
+  function setAggregatorAddressForDAI(AggregatorV3Interface _aggregator) internal {
+    aggregatorDAI = AggregatorV3Interface(_aggregator);
+  }
+
+  function getPriceForBTC() internal view returns (int) {
     
     (
             /* uint80 roundID */,
@@ -17,7 +22,21 @@ abstract contract PriceFeed {
             /*uint startedAt*/,
             /*uint timeStamp*/,
             /*uint80 answeredInRound*/
-        ) = aggregator.latestRoundData();
+        ) = aggregatorBTC.latestRoundData();
     return answer;  // I might need to devide it by 1e18
   }
+
+  function getPriceForDAI() internal view returns (int) {
+    
+    (
+            /* uint80 roundID */,
+            int answer,             // What is the decimal of returned value? Is it 18? See Code walk note to understand better ⭐️
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = aggregatorDAI.latestRoundData();
+    return answer;  // I might need to devide it by 1e18
+  }
+
+
 }
--- src/Main.sol
+++ src/Main.sol
@@ -2,153 +2,116 @@
 pragma solidity 0.8.20;
 import './Aggregator/PriceFeed.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
-// This may be a issue that this is contract is taking ETH while adding cliquidity and also taking ETH while opening position. It might be better to have different 
-// contract only for taking liquidity.
+contract Main is Ownable, PriceFeed {
+  error Insufficient_balance();
+  error Insufficient_collateral_provided();
+  error Not_a_trader(address trader);
+  error Not_LP();
+  error Insufficient_amount_to_withdraw();
+  error Max_Utilization_Limit_Reached();
 
-contract Main is PriceFeed, Ownable {
-  error Provide_Enough_Eth();
-  error Not_LP(address _caller);
-  error Liquidity_Withdrawal_Failed(address _for, uint256 _amount);
-  error Zero_Address();
-  error Not_Minimum_Collateral_Deposited();
-  error Malicious_Call(address _by);
-  error Profit_Sending_Failed_For_Closing_Long_Position();
-  error Refunding_Failed_For_Closing_Long(address _for, int256 _value);
-  error Reached_Liquidity_Reserve_Limit(uint256 _liquidity);
-
-  // tracking how much liquidity a LP is providing
-  mapping(address => uint256) LPs;
-  mapping(address => bool) isLP;
-  mapping(address => uint256) identifyTradersByDirection;
-
-  uint256 allowBorrowPercentage = 99;
-  uint256 allowedMinCollateral = 0.5 ether;
-  uint256 liquidity;
-  uint256 openInterest;
-  uint256 maxUtilizationPercentage = 60;
-
-  enum TradeStatus {
-    Null,
-    Open,
-    Close
-  }
-
-  enum PositionStatus {
-    Null,
-    Long,
-    Short
-  }
+  IERC20 private dai;
+  uint256 minCollateral = 100;
+  uint256 decimal = 1e18;
+  int256 usdDecimal = 1e8;
+  int256 daiDecimal = 1e18;
+  uint256 leverage = 10;
+  uint256 LIQUIDITY;
+  uint256 OPEN_INTEREST;
+  uint256 MAX_UTILIZATION_LIMIT_PERCENTAGE = 30;
 
   struct TraderUtils {
     uint256 collateral;
-    uint256 borrowedAmount;
-    int256 currentValue;
-    TradeStatus tradeStatus;
-    PositionStatus positionStatus;
+    int256 sizeInBTC;
+    int256 sizeInUSD;
   }
 
-  mapping(address => TraderUtils) addressToTraderUtils;
+  mapping(address => TraderUtils) traderUtils;
+  mapping(address => uint256) isTrader; // if set to 1 means the address was validated as trader
+  mapping(address => uint256) LpToFunded;
 
   constructor(address initialOwner) Ownable(initialOwner) {}
 
-  function depositLiquidity() external payable {
-    if (msg.value != 0.05 ether) {
-      revert Provide_Enough_Eth();
+  //LPs can add liquidity
+  function addLiquidity(uint _amount) external {
+    uint256 amount = _amount / decimal;
+    if (dai.balanceOf(msg.sender) < _amount) {
+      revert Insufficient_balance();
     }
-    LPs[msg.sender] = msg.value;
-    liquidity += msg.value;
+    dai.transferFrom(msg.sender, address(this), amount);
+    LpToFunded[msg.sender] += amount;
+    LIQUIDITY += amount;
   }
 
-  function withdrawLiquidity() external {
-    if (!isLP[msg.sender]) {
-      revert Not_LP(msg.sender);
+  // can open perpetual position
+  // WEI -> DAI -> USD -> BTC
+  function openPosition(uint256 amount) external {
+    uint256 _amount = amount / decimal; // As input is in DAI form we are dividing it by 1e18
+    if (_amount < minCollateral) {
+      revert Insufficient_collateral_provided();
     }
-
-    if (openInterest < (liquidity * maxUtilizationPercentage) / 100) {
-      // LPs can't withdraw their liquidity if liquidity reserve limit reached
-      revert Reached_Liquidity_Reserve_Limit(liquidity);
+    if (dai.balanceOf(msg.sender) < _amount) {
+      revert Insufficient_balance();
     }
-
-    uint256 _value = LPs[msg.sender];
-    (bool success, ) = payable(msg.sender).call{value: _value}('');
-    if (!success) {
-      revert Liquidity_Withdrawal_Failed(msg.sender, _value);
-    }
+    dai.transferFrom(msg.sender, address(this), _amount);
+    uint256 size = _amount * 5;
+    int256 currentBTCPriceInUSD = getPriceForBTC() / usdDecimal; // here we get BTC price in USD, dividing it by 1e8 to get actual value
+    int256 daiToUSD = getPriceForDAI() / daiDecimal; // here we get DAI price in USD, dividing it by 1e8 to get actual value
+    int256 sizeInUSD = int(size) * daiToUSD;
+    int256 sizeInBTC = currentBTCPriceInUSD / int(sizeInUSD);
+    traderUtils[msg.sender] = TraderUtils(_amount, sizeInBTC, sizeInUSD);
+    isTrader[msg.sender] = 1;
+    OPEN_INTEREST += uint256(size);
   }
 
-  function setPriceFeedAddress(AggregatorV3Interface _address) external onlyOwner {
-    setAggregatorAddress(_address);
+  // can increase the size of the position
+  function increaseSize(uint256 _amount) external onlyTrader {
+    uint256 amount = _amount / decimal;
+    if (dai.balanceOf(msg.sender) < amount) {
+      revert Insufficient_balance();
+    }
+    int256 daiToUsd = getPriceForDAI() / daiDecimal;
+    int256 sizeToBeIncreasedInUSD = daiToUsd * int(amount);
+    int256 btcCurrentPriceInUSD = getPriceForBTC() / usdDecimal;
+    int256 sizeToBeIncreasedInBTC = btcCurrentPriceInUSD / sizeToBeIncreasedInUSD; // if this USD to 1 BTC then  in this USD to how much BTC?
+    traderUtils[msg.sender].sizeInBTC += sizeToBeIncreasedInBTC;
+    traderUtils[msg.sender].sizeInUSD += sizeToBeIncreasedInUSD;
   }
 
-  function openPositionForLong() external payable isAddress(msg.sender) {
-    if (msg.value < allowedMinCollateral) {
-      revert Not_Minimum_Collateral_Deposited();
+  // can increase collateral
+  function increaseCollateral(uint _amount) external onlyTrader {
+    uint256 amount = _amount / decimal;
+    if (dai.balanceOf(msg.sender) < amount) {
+      revert Insufficient_balance();
     }
-    int256 btcToEth = getPrice(); // getting 1 BTC = ? ETH
-    uint256 borrowedAmount = (msg.value * allowBorrowPercentage) / 100; // this will be credited to traders to trade
-    int256 currentValue = int256(borrowedAmount) / btcToEth; // current value in terms of BTC
-    addressToTraderUtils[msg.sender] = TraderUtils(
-      msg.value,
-      borrowedAmount,
-      currentValue,
-      TradeStatus.Open,
-      PositionStatus.Long
-    );
-    identifyTradersByDirection[msg.sender] = 1;
-    openInterest += borrowedAmount; // recording openInterest in ETH
+    traderUtils[msg.sender].collateral += amount;
   }
 
-  function closePositionForLong() external isAddress(msg.sender) {
-    if (identifyTradersByDirection[msg.sender] != 1) {
-      revert Malicious_Call(msg.sender);
+  //withdraw liquidity
+  function withdrawLiquidity(uint _amount) external {
+    uint256 amount = _amount / decimal;
+    if (LpToFunded[msg.sender] == 0) {
+      revert Not_LP();
+    } else if (LpToFunded[msg.sender] < amount) {
+      revert Insufficient_amount_to_withdraw();
     }
-    delete addressToTraderUtils[msg.sender];
-    int256 currentPrice = getPrice(); // Current BTC value in terms of ETH
-    uint256 borrowedAmount = addressToTraderUtils[msg.sender].borrowedAmount; // Getting borrowed amount while opening the positio
-    int256 currentValue = int256(borrowedAmount) / currentPrice; // the valu in terms of eth
-    int256 priceDifference = currentValue - addressToTraderUtils[msg.sender].currentValue; // PnL
-    if (currentValue > addressToTraderUtils[msg.sender].currentValue) {
-      addressToTraderUtils[msg.sender].tradeStatus = TradeStatus.Close;
-      // Deduct from LPs ⭐️
-      liquidity - uint256(priceDifference);
-      openInterest -= borrowedAmount; // Updating open interest after closing the position
-      (bool success, ) = payable(msg.sender).call{value: uint256(priceDifference)}('');
-      if (!success) {
-        revert Profit_Sending_Failed_For_Closing_Long_Position();
-      }
-    } else if (currentValue < addressToTraderUtils[msg.sender].currentValue) {
-      addressToTraderUtils[msg.sender].tradeStatus = TradeStatus.Close;
-      int256 collateral = int256(addressToTraderUtils[msg.sender].collateral);
-      int256 refundableAmountWithoutFee = collateral - priceDifference;
-      int256 refundableAmountAfterFeeDeduction = refundableAmountWithoutFee -
-        int256(addressToTraderUtils[msg.sender].collateral) /
-        100; // liquidityFee is 1% of collateral
-      (bool success, ) = payable(msg.sender).call{value: uint256(refundableAmountAfterFeeDeduction)}('');
-      if (!success) {
-        revert Refunding_Failed_For_Closing_Long(msg.sender, refundableAmountAfterFeeDeduction);
-      }
+    if (OPEN_INTEREST < (LIQUIDITY * MAX_UTILIZATION_LIMIT_PERCENTAGE) / 100) {
+      revert Max_Utilization_Limit_Reached();
     }
+    dai.transferFrom(address(this), msg.sender, amount);
   }
 
-  // traders can increase size & collateral of perpetual position
-  function increasePositionSizeAndCollateralForLong() external payable isAddress(msg.sender) {
-    if (identifyTradersByDirection[msg.sender] != 1) {
-      revert Malicious_Call(msg.sender);
-    }
-
-    uint256 borrowedAmount = (msg.value * allowBorrowPercentage) / 100;
-    addressToTraderUtils[msg.sender].collateral += msg.value;
-    addressToTraderUtils[msg.sender].borrowedAmount += borrowedAmount; // size increased
+  // helper functions
+  function setDaiAddress(IERC20 _dai) external onlyOwner {
+    dai = IERC20(_dai);
   }
 
-  modifier isAddress(address _addr) {
-    if (_addr != address(0)) {
-      revert Zero_Address();
+  modifier onlyTrader() {
+    if (isTrader[msg.sender] != 1) {
+      revert Not_a_trader(msg.sender);
     }
     _;
   }
 }
-// Net amount of liquidity pool was not tracked.
-// During calculation of net PnL is it necessary to use USD here:
-// (currentValueOfPosition*openInterestInTermsOfETH) - openInterestInTermsOfUSD
